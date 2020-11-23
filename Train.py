2232a778ff7087332d8e73f587012ece362bb3c2
PhiCtl <44364660+PhiCtl@users.noreply.github.com>
2020-11-23 21:00:03 +0100

    Added metrics

--- Train.py
+++ Train.py
@@ -1,8 +1,9 @@
 import numpy as np
+from tensorflow.keras.metrics import Precision, Recall, Accuracy
 
 #need to write evaluate_model here:
-def evaluate_model:
-    raise NotImplementedError
+"""def evaluate_model:
+    raise NotImplementedError"""
 
 def summarize_scores(scores,score_name):
     """Nice printing function for 1 score only...
@@ -25,4 +26,37 @@ def run_exp(x_tr, y_tr, x_val, y_val, repeats=3, gamma = 1, node = 100 ):
         print('>#%d: %.3f' %(1, score1))
         scores.append(score1)
     summarize_scores(scores)
+    
+def F1_score(y_true, y_pred, class_weights):
+    """Compute the F1_score for each label, weighted with its absolute proportion
+    Argument: ground truth label Y (3D), prediction (3D), class_weights (inverse of proportion)
+    Return: array of F1_score for each label"""
+    m = Precision()
+    n = Recall()
+    F1_score_per_label = []
+    for i in range(8):
+        m.update_state(y_true[:,:,i], y_pred[:,:,i])
+        n.update_state(y_true[:,:,i], y_pred[:,:,i])
+        p = n.result().numpy()
+        r = m.result().numpy()
+        if p + r == 0:
+            F1_score = 0
+        else:
+            F1_score = 2* p * r / (p + r) #F1 score computed as harminic mean of precision and recall
+            F1_score_per_label.append(F1_score*class_weights[i])
+    return F1_score_per_label
+    
+def Acc(y_true, y_pred, class_weights):
+    """Compute the accuracy for each label, weighted with its absolute proportion
+    Argument: ground truth label Y (3D), prediction (3D), class_weights (inverse of proportion)
+    Return: array of accuracy for each label"""
+    a = Accuracy()
+    Acc_per_label = []
+    for i in range(8):
+        a.update_state(y_true[:,:,i], y_pred[:,:,i])
+        acc = a.result().numpy()
+        Acc_per_label.append(acc*class_weights[i])
+    return Acc_per_label
+    
+    
 
