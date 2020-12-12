195f23838823bb4689a5d4372e3a8a76a4bbd688
PhiCtl <44364660+PhiCtl@users.noreply.github.com>
2020-12-12 17:33:18 +0100

    Run function to fill in

---
+++ run.py
@@ -0,0 +1,24 @@
+from Load import load_test_data
+from Train import predict
+
+#load data
+print("Folder of pose tracking and annotation files (absolute path from data_fly folder): ")
+folder = input()
+X,Y = load_test_data(folder)
+
+print(
+'''
+Enter flag for model prediction: \n 
+##################################
+-> LSTM         : to make predictions with best LSTM model
+-> TCN          : to make predictions with best TCN model
+-> Random Forest: to make predictions with best Random Forest model
+##################################
+'''
+)
+flag = input()
+
+#make predictions
+predict(X,Y, flag)
+
+
